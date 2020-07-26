1ff40ee42a177f025016c727ff27aaf1b8828f54
Sam <samsong974@gmail.com>
2020-07-26 01:45:06 -0700

    draw shapes successful

--- overviewchart.js
+++ overviewchart.js
@@ -6,11 +6,11 @@ async function overviewLine() {
     var target = "China"
 
     // Get data from Only China
-    const data = await d3.csv("http://127.0.0.1:5500/USTradeWar/data/Test.csv", function (d) {
+    const data = await d3.csv("http://127.0.0.1:5500/USTradeWar/data/USA-Trade2010-v2.csv", function (d) {
         if (d.Country == "China")
             return {
-                //exports: parseInt(d['Total Exports Value ($US)'].replace(/\,/g, '')),
-                imports: parseInt(d['Customs Import Value (Gen) ($US)'].replace(/\,/g, '')),
+                exports: parseInt(d['Total Exports Value ($US)'].replace(/\,/g, '')),
+                imports: parseInt(d['Customs Import Value (Gen) ($US)'].replace(/\,/g, ''))
             }
 
     });
@@ -34,15 +34,14 @@ async function overviewLine() {
 
     var line = d3.line()
         .x(function (d, i) { return i; }) // set the x values for the line generator
-        .y(function (d) { return 0; });
+        .y(function (d) { return d.imports; });
 
     d3.select('svg')
         .append("g")
-        .attr("transform", "translate(" + margin + "," + margin + ")")
+        .attr("transform", "translate(" + 100 + "," + margin + ")")
         .selectAll('path')
         .enter().append('path')
         .datum([data])
-        .attr("fill", "none")
         .attr("stroke", "steelblue")
         .attr("stroke-width", 1.5)
         .attr("d", line);
@@ -57,61 +56,6 @@ async function overviewLine() {
         .attr("cx", function (d, i) { return x(i) })
         .attr("cy", function (d) { return y(d.imports) })
         .attr("r", 2)
-
-    d3.select('svg').append("g")
-        .attr("transform", "translate(" + margin + "," + margin + ")")
-        .call(yAxis);
-    d3.select('svg').append("g")
-        .attr("transform", "translate(" + margin + "," + (height + margin) + ")")
-        .call(xAxis);
-
-}
-
-async function overviewPoints() {
-    const margin = 50;
-    const height = 400;
-    var width = 500;
-    var target = "China"
-
-    // Get data from Only China
-    const data = await d3.csv("http://127.0.0.1:5500/USTradeWar/data/USA-Trade2010-v2.csv", function (d) {
-        if (d.Country == "China")
-            return {
-                exports: d['Total Exports Value ($US)'],
-                imports: d['Customs Import Value (Gen) ($US)'],
-            }
-
-    });
-    console.log(data);
-
-    /*var x = d3.scaleTime()
-        .domain(d3.extent(data, function (d) { return d.time; }))
-        .range([0, width]);*/
-    var x = d3.scaleLinear()
-        .domain([0, d3.max(data, function (d) { return d.exports; })])
-        .range([height, 0]);
-
-    var y = d3.scaleLinear()
-        .domain([0, d3.max(data, function (d) { return d.imports; })])
-        .range([height, 0]);
-
-    var xAxis = d3.axisBottom(x)
-        .tickFormat(d3.format("~s"));
-    var yAxis = d3.axisLeft(y)
-        .tickFormat(d3.format("~s"))
-
-    d3.select('svg')
-        .append("g")
-        .attr("transform", "translate(" + margin + "," + margin + ")")
-        .selectAll('circle')
-        .data(data)
-        .enter()
-        .append('circle')
-        .attr("cx", function (d) { return x(d.exports); })
-        .attr("cy", function (d) { return y(d.imports); })
-        .attr("r", function (d) { return 2; });
-
-
 
     d3.select('svg').append("g")
         .attr("transform", "translate(" + margin + "," + margin + ")")
