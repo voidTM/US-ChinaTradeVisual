03b73b78e855892379a9e1136a365181cdfa3e34
Sam <samsong974@gmail.com>
2020-07-25 23:31:58 -0700

    scales broken

--- overviewchart.js
+++ overviewchart.js
@@ -1,33 +1,37 @@
-async function overview() {
+// first and initial chart
+async function overviewLine() {
     const margin = 50;
-    const height = 200;
-    var width = 200;
+    const height = 400;
+    var width = 500;
     var target = "China"
-  
-    const data = await d3.csv("file://./data/USA-Trade2010.csv", function(data){
-        return d.Country == "China"
-        });
+
+    // Get data from Only China
+    const data = await d3.csv("http://127.0.0.1:5500/USTradeWar/data/USA-Trade2010.csv", function (d) {
+        if (d.Country == "China")
+            return {
+                country: d.Country,
+                time: d.Time,
+                exports: d['Total Exports Value ($US)'],
+                imports: d['Customs Import Value (Gen) ($US)'],
+            }
+
+    });
     console.log(data);
-    console.log(data.Country);
-    var scale = d3.scaleLog()
-        .base(10)
-        .domain([10, 150]);
 
-    var xScale = d3.scaleLog()
-        .base(10)
-        .domain([10, 150])
-        .range([0, width]);
-
-    var yScale = d3.scaleLog()
-        .base(10)
-        .domain([10, 150])
+    /*var x = d3.scaleTime()
+        .domain(d3.extent(data, function (d) { return d.time; }))
+        .range([0, width]);*/
+    var x = d3.scaleLinear()
+        .domain([0, d3.max(data, function (d) { return d.exports; })])
         .range([height, 0]);
 
-    var xAxis = d3.axisBottom(xScale)
-        .tickValues(axisticks)
+    var y = d3.scaleLog()
+        .domain([0, d3.max(data, function (d) { return d.imports; })])
+        .range([height, 0]);
+
+    var xAxis = d3.axisBottom(x)
         .tickFormat(d3.format("~s"));
-    var yAxis = d3.axisLeft(yScale)
-        .tickValues(axisticks)
+    var yAxis = d3.axisLeft(y)
         .tickFormat(d3.format("~s"))
 
     d3.select('svg')
@@ -37,8 +41,69 @@ async function overview() {
         .data(data)
         .enter()
         .append('path')
-        .attr("x", function (d) { return xScale(d.Time); })
-        .attr("y", function (d) { return yScale(d.country); });
+        .attr("stroke", "steelblue")
+        .attr("stroke-width", 1.5)
+        .attr("d", d3.line()
+            .x(function (d) { return x(d.exports); })
+            .y(function (d) { return y(d.imports); }));
+
+
+    d3.select('svg').append("g")
+        .attr("transform", "translate(" + margin + "," + margin + ")")
+        .call(yAxis);
+    d3.select('svg').append("g")
+        .attr("transform", "translate(" + margin + "," + (height + margin) + ")")
+        .call(xAxis);
+
+}
+
+async function overviewPoints() {
+    const margin = 50;
+    const height = 400;
+    var width = 500;
+    var target = "China"
+
+    // Get data from Only China
+    const data = await d3.csv("http://127.0.0.1:5500/USTradeWar/data/USA-Trade2010.csv", function (d) {
+        if (d.Country == "China")
+            return {
+                country: d.Country,
+                time: d.Time,
+                exports: d['Total Exports Value ($US)'],
+                imports: d['Customs Import Value (Gen) ($US)'],
+            }
+
+    });
+    console.log(data);
+
+    /*var x = d3.scaleTime()
+        .domain(d3.extent(data, function (d) { return d.time; }))
+        .range([0, width]);*/
+    var x = d3.scaleLinear()
+        .domain([0, d3.max(data, function (d) { return d.exports; })])
+        .range([height, 0]);
+
+    var y = d3.scaleLinear()
+        .domain([0, d3.max(data, function (d) { return d.imports; })])
+        .range([height, 0]);
+
+    var xAxis = d3.axisBottom(x)
+        .tickFormat(d3.format("~s"));
+    var yAxis = d3.axisLeft(y)
+        .tickFormat(d3.format("~s"))
+
+    d3.select('svg')
+        .append("g")
+        .attr("transform", "translate(" + margin + "," + margin + ")")
+        .selectAll('circle')
+        .data(data)
+        .enter()
+        .append('circle')
+        .attr("cx", function (d) { return x(d.exports); })
+        .attr("cy", function (d) { return y(d.imports); })
+        .attr("r", function (d) { return 2; });
+
+
 
     d3.select('svg').append("g")
         .attr("transform", "translate(" + margin + "," + margin + ")")
