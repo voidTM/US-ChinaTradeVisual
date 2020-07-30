15a3c4c8d586b8d940275a2d4379c33fc109c184
Sam <samsong974@gmail.com>
2020-07-30 16:11:03 -0700

    added annotations for chart 1

--- src/charts2.js
+++ src/charts2.js
@@ -1,13 +1,44 @@
 
 
 var margin = { top: 10, right: 50, bottom: 50, left: 100 }
-    ,width = 1000 - margin.right - margin.left
-    ,height = 500 - margin.top - margin.bottom;
+    , width = 1000 - margin.right - margin.left
+    , height = 500 - margin.top - margin.bottom;
+
+const colorScheme = {
+    imports: "rgb(223, 63, 63)",
+    balance: "blueviolet",
+    deficit: "blueviolet",
+    exports: "green"
+}
+
+var currentScene = 1;
+
+// Scales
+var xTime;
+var y;
+
+var balanceLine = d3.line()
+    .x(function (d) { return xTime(yearDate(d.time)); })
+    .y(function (d) { return y(d.deficit) })
+
+var importsLine = d3.line()
+    .x(function (d, i) { return xTime(yearDate(d.time)); })
+    .y(function (d) { return y(d.imports); }) // set the y values for the line generator 
+
+var exportsLine = d3.line()
+    .x(function (d, i) { return xTime(yearDate(d.time)); })
+    .y(function (d) { return y(d.exports); }) // set the y values for the line generator 
+
 // Date time format parsers
 var yearDate = d3.timeParse("%Y");
 var monthYear = d3.timeParse("%B %Y");
 
+// Data from csvs
 var csvData;
+var monthlyData;
+
+// Annotations
+var annotations = []
 
 function wrap(text, width) {
     text.each(function () {
@@ -34,19 +65,48 @@ function wrap(text, width) {
 }
 
 
-function drawAxes(svg, xAxes, yAxes) {
+function drawAxes(svg, x, y) {
+
+    var xAxes = d3.axisBottom(x).ticks(10);
+    var yAxes = d3.axisLeft(y)
+        .tickFormat(function (d) { return "$" + (d / 1000000000) + " bn" })
+        .ticks(10)
+        .tickSize(-width);
+
 
     svg.append("g")
         .attr("class", "x-axis")
         .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(xAxes)); // Create an axis component with d3.axisBottom
+        .call(xAxes); // Create an axis component with d3.axisBottom
 
     svg.append("g")
         .attr("class", "y-axis")
-        .call(d3.axisLeft(yAxes).tickFormat(function (d) { return d / 1000000 + "M" })); // Create an axis component with d3.axisLeft
+        .call(yAxes); // Create an axis component with d3.axisLeft
+}
+
+function drawLine(path, line, color) {
 
 }
 
+function annotateArea(svg, note) {
+    t = svg.append("g")
+        .attr("class", "annotation")
+        .attr("transform", "translate(" + note.x + "," + note.y + ")");
+
+    t.append("rect")
+        .attr("class", "rect")
+        .attr("x", 0)
+        .attr("y", 0)
+        .attr("width", note.width) // 3 years
+        .attr("height", height);
+
+    t.append("text")
+        .attr("x", 0)
+        .attr("y", 0)
+        .attr("dy", 1)
+        .text(note.label)
+        .call(wrap, note.width)
+}
 
 async function init() {
     // move querying data to here?
@@ -69,15 +129,16 @@ async function drawChart1() {
 
     });
 
+    csvData = data;
 
     var yearDate = d3.timeParse("%Y")
-    var xTime = d3.scaleTime()
+    xTime = d3.scaleTime()
         //scale time does not seem to undertand the format?
         .domain(d3.extent(data, function (d) { return yearDate(d.time); })) //fail?
         .range([0, width]);
 
 
-    var y = d3.scaleLinear()
+    y = d3.scaleLinear()
         .domain([0, d3.max(data, function (d) { return d.imports; })])
         .range([height, 0]);
 
@@ -93,8 +154,9 @@ async function drawChart1() {
         .x(function (d, i) { return xTime(yearDate(d.time)); })
         .y(function (d) { return y(d.exports); }) // set the y values for the line generator 
 
-    var svg = d3.select("#slide1").select("svg")
+    var svg = d3.select(".container").select("svg")
         .append("g")
+        .attr("class", "slide-viz")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 
@@ -102,28 +164,110 @@ async function drawChart1() {
     //Draw line imports
 
     svg.append("g")
+        .attr("class", "scene1")
+        .attr("opacity", 1)
         .append("path")
-        .datum(data) // 10. Binds data to the line 
-        .attr("class", "balance line") // Assign a class for styling 
-        .attr("d", balanceLine); // 11. Calls the line generator 
+        .datum(data)
+        .attr("class", "line")
+        .attr("d", balanceLine)
+        .attr("stroke", colorScheme["deficit"]);
 
     svg.append("g")
-        .append("path")
-        .datum(data) // 10. Binds data to the line 
+        .attr("class", "scene2")
         .attr("opacity", 0)
-        .attr("class", "imports line") // Assign a class for styling 
-        .attr("d", importsLine); // 11. Calls the line generator 
+        .append("path")
+        .datum(data)
+        .attr("class", "line imports")
+        .attr("d", balanceLine)
+        .attr("stroke", colorScheme["imports"]);
 
     svg.append("g")
-        .append("path")
-        .datum(data) // 10. Binds data to the line 
-        .attr("class", "exports line") // Assign a class for styling 
+        .attr("class", "scene2")
         .attr("opacity", 0)
-        .attr("d", exportsLine); // 11. Calls the line generator 
-
+        .append("path")
+        .datum(data)
+        .attr("class", "line exports")
+        .attr("d", balanceLine)
+        .attr("stroke", colorScheme["exports"]);
 
     //draw 2018+ fade annotation
 
+    /**
+     * Attempt at annotations
+     */
+
+    var notes = svg.append("g")
+        .attr("class", "annotation");
+
+    annotations.push({
+        label: "Financial Recession",
+        y: 0,
+        x: xTime(new Date("12/01/2007")), //position the x based on an x scale
+        width: xTime(new Date("1/01/2010")) - xTime(new Date("12/01/2007")),
+        validScenes: [1, 2, 3]
+    })
+
+    annotations.push({
+        label: "US Trade War with China",
+        y: 0,
+        x: xTime(new Date("01/01/2018")), //position the x based on an x scale
+        width: xTime(new Date("1/01/2005")),
+        validScenes: [1, 2, 3]
+    })
+
+    annotateArea(svg, annotations[0]);
+    annotateArea(svg, annotations[1]);
+    /*
+    notes.append("rect")
+        .attr("class", "rect")
+        .attr("x", function (d) {
+            return xTime(new Date(2008, 0));
+        })
+        .attr("y", 0)
+        .attr("width", function (d) {
+            return xTime(new Date(2005, 0)); // 3 years
+        })
+        .attr("height", height);
+
+    notes.append("text")
+        .attr("x", function (d) {
+            return xTime(new Date(2008, 0));
+        })
+        .attr("y", 0)
+        .attr("width", function (d) {
+            return xTime(new Date(2005, 0)); // 3 years
+        })
+        .text("Economic Recession");
+    */
+
+}
+
+
+// transition from scene 1 to 2
+// Transitions should hide the previous scene and the scene after
+function scene3() {
+    d3.selectAll(".scene1")
+        .transition()
+        .duration(100)
+        .attr("opacity", 0);
+    d3.selectAll(".scene2").selectAll(".line.imports")
+        .transition()
+        .duration(800)
+        .attr("opacity", 1)
+        .attr('d', importsLine);
+
+    d3.selectAll(".scene2").selectAll(".line.exports")
+        .transition()
+        .duration(800)
+        .attr("opacity", 1)
+        .attr('d', exportsLine);
+
+    d3.selectAll(".scene2")
+        .transition()
+        .attr("opacity", 1)
+        .duration(0)
+
+    currentScene = 3;
 }
 
 
@@ -187,14 +331,12 @@ async function drawChart2() {
         .call(d3.axisBottom(x)) // Create an axis component with d3.axisBottom
         .selectAll("text")
         .attr("y", 20)
-        .style("font-size", "small");
-        //.call(wrap, x.step());
+        .style("font-size", "small")
+        .call(wrap, x.step());
 
     svg.append("g")
         .attr("class", "y-axis")
         .call(d3.axisLeft(y).ticks(5, "s")); // Create an axis component with d3.axisLeft
-
-
 
     svg.append("g")
         .selectAll("rect")
